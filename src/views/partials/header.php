edb0ac9af97da6fa79c5092ca656aab005a0b512
dilsoncampelo10 <dilsono316@gmail.com>
2022-09-24 14:19:25 -0300

    Criando header

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,1 +1,26 @@
-<h1>Header</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LuaBook</title>
+    <link rel="stylesheet" href="<?=$base?>/assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav class="navbar">
+        <div class="navbar-content">
+            <a href="<?=$base?>"><img src="<?=$base?>/assets/images/luabook-logo.png" alt="logo luabook" class="logo"></a>
+            
+            <form action="" method="get">
+                <input type="search" name="search" id="search" placeholder="Pesquisar">
+            </form>
+        </div>
+        <ul class="navbar-content">
+            <li><a href="">Perfil</a></li>
+            <li><a href=""><img src="<?=$base?>/assets/images/user.png" alt="perfil"> Lígia Bianca</a></li>
+            <li><a href="">Sair</a></li>
+        </ul>
+    </nav>
+</header>
